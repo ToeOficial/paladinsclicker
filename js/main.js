26f36e01c8a1897a2d34c49cf0ab3b01da4059a5
duckey <tomekch6@gmail.com>
2016-10-11 17:59:30 +0200

    Fix loading

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-//On page load
+//On DOM load
 $(function() {
     $('.pc-rightbar').mCustomScrollbar({
         axis: 'y',
         scrollbarPosition: 'inside',
         theme: 'minimal'
     });
+});
 
-
-
-
+//On full page load
+$(window).on("load", function() {
     $('.loadingOverlay').fadeOut(500);
 });
