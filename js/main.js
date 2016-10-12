b82c77221b8697f44981c4c6ff3cf0259acecdbc
duckey <tomekch6@gmail.com>
2016-10-12 19:37:46 +0200

    Add function for getting random champion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const CHAMPION_LIST = ['Androxus', 'Barik', 'Bomb King', 'Buck', 'Cassie', 'Drogoz', 'Evie', 'Fernando', 'Grohk', 'Grover', 'Kinessa', 'Makoa', 'Pip', 'Ruckus', 'Skye', 'Viktor', 'Ying'];
+
+
+function randomizeChampion() {
+    return CHAMPION_LIST[Math.floor(Math.random()*CHAMPION_LIST.length())];
+}
+
 //On DOM load
 $(function() {
     $('.pc-rightbar').mCustomScrollbar({
@@ -5,9 +12,10 @@ $(function() {
         scrollbarPosition: 'inside',
         theme: 'minimal'
     });
+    
 });
 
 //On full page load
-$(window).on("load", function() {
+$(window).on('load', function() {
     $('.loadingOverlay').fadeOut(500);
 });
