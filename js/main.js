835072607fd7c5101d7f23397bb074e7afd1fdd2
duckey <tomekch6@gmail.com>
2016-10-13 16:09:53 +0200

    Register clicks + comments

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+//     ***** INIT *****
+
+//Champion list
+//UPDATE WHEN NEW CHAMPION COMES OUT
 const CHAMPION_LIST = ['Androxus', 'Barik', 'Bomb King', 'Buck', 'Cassie', 'Drogoz', 'Evie', 'Fernando', 'Grohk', 'Grover', 'Kinessa', 'Makoa', 'Pip', 'Ruckus', 'Skye', 'Viktor', 'Ying'];
 var activeChampion = randomizeChampion();
 
+//Get random champion from the list
 function randomizeChampion() {
     return CHAMPION_LIST[Math.floor(Math.random()*CHAMPION_LIST.length)];
 }
+
+//Function running on each click
+function click() {
+    console.log('Click!');
+}
+
+
+//     ***** EVENTS *****
+//On champion icon left or middle click
+$('.champion-icon').click(function() {
+    click();
+});
 
 //On DOM load
 $(function() {
@@ -23,4 +40,15 @@ $(window).on('load', function() {
         //On fade out complete
         $(this).remove();
     });
+});
+
+//On champion wrapper right click
+$('.champion-wrapper').contextmenu(function() {
+    return false;
+});
+
+//On champion icon right click
+$('.champion-icon').contextmenu(function() {
+    click();
+    return false;
 });
