1df3f41c0d560a508a0c9d213b60aaca071c64d1
duckey <tomekch6@gmail.com>
2016-10-14 17:13:54 +0200

    Add combat

--- js/main.js
+++ js/main.js
@@ -3,16 +3,43 @@
 //Champion list
 //UPDATE WHEN NEW CHAMPION COMES OUT
 const CHAMPION_LIST = ['Androxus', 'Barik', 'Bomb King', 'Buck', 'Cassie', 'Drogoz', 'Evie', 'Fernando', 'Grohk', 'Grover', 'Kinessa', 'Makoa', 'Pip', 'Ruckus', 'Skye', 'Viktor', 'Ying'];
-var activeChampion = randomizeChampion();
+var activeChampion = '';
+
+var championHP = 10;
+var maxChampionHP = 10;
+var dmg = 1;
 
 //Get random champion from the list
 function randomizeChampion() {
-    return CHAMPION_LIST[Math.floor(Math.random()*CHAMPION_LIST.length)];
+    activeChampion = CHAMPION_LIST[Math.floor(Math.random()*CHAMPION_LIST.length)];
+
+    $('.champion-icon').attr('src', './img/champicons/'+activeChampion+'.png');
+    $('.champion-icon').attr('alt', activeChampion);
+    $('.champion-name').html(activeChampion);
 }
 
+function updateHP() {
+    $('.health-progress').animate({
+        width: (274-((championHP/maxChampionHP)*274))
+    }, 100);
+    if (championHP<=0) {
+        $('.champion-icon').effect('explode', {pieces: 32}, 500, function() {
+            randomizeChampion();
+            $(this).show();
+
+            $('.health-progress').animate({
+                width: (274-((championHP/maxChampionHP)*274))
+            }, 100);
+        });
+        //randomizeChampion();
+        championHP = maxChampionHP;
+    }
+}
 //Function running on each click
 function click() {
     console.log('Click!');
+    championHP-=dmg;
+    updateHP();
 }
 
 //On full page load
@@ -32,10 +59,7 @@ $(function() {
         theme: 'minimal'
     });
 
-    $('.champion-icon').attr('src', './img/champicons/'+activeChampion+'.png');
-    $('.champion-icon').attr('alt', activeChampion);
-    $('.champion-name').html(activeChampion);
-
+    randomizeChampion();
 
     //     ***** EVENTS *****
     //On champion icon left or middle click
