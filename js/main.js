c8f0604d75093c3a2e4cd1b49d9eecf968855e43
duckey <tomekch6@gmail.com>
2016-10-17 19:31:49 +0200

    Add items

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var dps = 0;
 
 var gold = 0;
 var goldPerKill = 3;
-var gps = 0;
+var topUnlocked = 0;
 
 var level = 1;
 var oldXp = 0;
@@ -65,7 +65,7 @@ function updateXp() {
         oldXp=0;
         xp=(xp-maxXp);
         maxXp=Math.floor(1.3*maxXp);
-        maxChampionHP+=2;
+        maxChampionHP+=5;
         xpPerKill=Math.floor(1.05*xpPerKill);
         $('.level-text').effect('highlight', { color: '#54E9E6'}, 1500);
         $('.level-text').html(level);
@@ -116,6 +116,70 @@ function click() {
     updateHP();
 }
 
+//     ***** ITEMS *****
+//"Class" handling items
+function Item(id, name, type, bonus, cost, req, unl) {
+    this.id = id; //Unique short string without any spaces
+    this.name = name; //Full name displayed on GUI
+    this.type = type; //Item type:
+    //1: DMG
+    //2: DPS
+    //3: GOLDPERKILL
+    this.bonus = bonus; //Stat amount you get from item
+    this.cost = cost; //Item cost
+    this.level = 0; //Starting level (by default 0, because we don't have it unlocked)
+    this.image = './img/items/' + id + '.png'; //Image stored in ./img/items/ with .png extension
+    this.costid = '#cost-' + id; //Id of element containing item price
+    this.levelid = '#level-' + id; //Id of element containing item level
+    this.buyid = '#buy-' + id; //Id of buy button
+    this.req = req; //Required item (most of the time 1 less)
+    this.unl = unl; //Item number (starts at 1)
+
+    this.buy = function() {
+        if (this.cost <= gold) {
+            gold -= this.cost;
+            gold = Math.floor(gold);
+            $('#goldSpan').html(gold);
+            this.level += 1;
+            this.cost = Math.floor(this.cost * 1.5);
+            if (this.type == 1) {
+                dmg += this.bonus;
+                $('#damageSpan').html(dmg);
+            } else if (this.type == 2) {
+                dps += this.bonus;
+                $('#dpsSpan').html(dps);
+            } else if (this.type == 3) {
+                goldPerKill += this.bonus;
+                $('#gpkSpan').html(goldPerKill);
+            }
+            if(this.unl > topUnlocked) {
+                topUnlocked = this.unl;
+            }
+            //play sound
+            return true;
+        }
+        return false;
+    }
+
+    this.render = function() {
+        $(this.costid).html(this.cost);
+        $(this.levelid).html(this.level);
+        if (this.cost > gold) {
+            $(this.buyid).addClass('dis');
+        } else {
+            $(this.buyid).removeClass('dis');
+        }
+    }
+
+    Item.instances.push(this);
+}
+
+//Instances:
+Item.instances = [];
+
+var basicbow = new Item('basicbow', 'Cassie\'s Bow', 1, 1, 20, 0, 1);
+
+
 //On full page load
 $(window).on('load', function() {
     $('.loadingOverlay').delay(500).fadeOut(500, function() {
@@ -157,7 +221,6 @@ $(function() {
 
     //     ***** DPS AND GPS TIMER *****
     var dpstimer = setInterval(function() {
-        gold+=gps;
         championHP-=dps;
         $('#goldSpan').html(gold);
         updateHP();
