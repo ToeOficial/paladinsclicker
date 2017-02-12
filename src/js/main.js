308d0f31bcdec803926e3b03fdf772270e4027ab
duckey <tomekch6@gmail.com>
2017-02-12 14:56:11 +0100

    Add Meave

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@ Paladins​™ and Paladins™: Champions of the Realm​​™ are trademarks o
 //     ***** INIT *****
 //Champion list
 //UPDATE WHEN NEW CHAMPION COMES OUT
-const CHAMPION_LIST = ['Androxus', 'Barik', 'Bomb King', 'Buck', 'Cassie', 'Drogoz', 'Evie', 'Fernando', 'Grohk', 'Grover', 'Kinessa', 'Makoa',
-                       'Mal\'Damba', 'Pip', 'Ruckus', 'Skye', 'Sha Lin', 'Torvald', 'Tyra', 'Viktor', 'Ying'];
+const CHAMPION_LIST = ['Androxus', 'Barik', 'Bomb King', 'Buck', 'Cassie', 'Drogoz', 'Evie', 'Fernando', 'Grohk', 'Grover', 'Kinessa', 'Maeve',
+                       'Makoa', 'Mal\'Damba', 'Pip', 'Ruckus', 'Skye', 'Sha Lin', 'Torvald', 'Tyra', 'Viktor', 'Ying'];
 var activeChampion = '';
 
 //Current and max hp of the champion
