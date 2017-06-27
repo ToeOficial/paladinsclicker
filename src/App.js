e9a7d56e0e0217b8e5838cd9450f31e2db18ce6e
duckey <tomekch6@gmail.com>
2017-06-27 22:30:33 +0200

    Remove sample app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import css from './App.css';
+import css from './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className={css.App}>
-        <div className={css.header}>
-          <img src={logo} className={css.logo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={css.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={`${css.container} ${css.bg_glacier_keep}`}>
+        App
       </div>
     );
   }
