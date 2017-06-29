037b066e6c90b5e0ceb7d6374227aaaf0ee20c93
duckey <tomekch6@gmail.com>
2017-06-29 17:12:32 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import css from './App.scss';
+import logo from './assets/logo.png';
+
+const siteName = 'Paladins Clicker';
 
 class App extends Component {
   render() {
     return (
-      <div className={`${css.container} ${css.bg_glacier_keep}`}>
-        App
+      <div className={css.container}>
+        <header className={css.navbar}>
+          <div className={css.logo}>
+            <img src={logo} alt={siteName} className={css.logoImg} />
+            {siteName}
+          </div>
+          <div className={css.iconsContainer}>
+            <i className={`material-icons ${css.icon}`}>settings</i>
+            <i className={`material-icons ${css.icon}`}>person</i>
+          </div>
+        </header>
+        <div className={css.columnContainer}>
+          <div />
+          <div />
+          <div />
+        </div>
       </div>
     );
   }
